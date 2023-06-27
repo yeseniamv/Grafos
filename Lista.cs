17ec316265dcc183753aecaaceb49c72205045de
Yesenia <yeseniamv1995@gmail.com>
2023-06-26 18:05:47 -0700

    Agregar archivos de proyecto.

---
+++ Lista.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    internal class Lista
+    {
+        Nodo nodoInicial;
+        Nodo nodoActual;
+       
+
+        public Lista()
+        {
+            nodoInicial = new Nodo();
+        }
+
+        public string Llave()
+        {
+            string dato = string.Empty;
+            dato = nodoInicial.Siguiente.Valor;
+            
+            return dato;
+        }
+        public bool ValidaVacio()
+        {
+            if (nodoInicial.Siguiente == null)
+            {
+                return true;
+            }
+            return false;
+            //return nodoInicial.Siguiente == null;
+
+        }
+        public void VaciarLista()
+        {
+            nodoInicial.Siguiente = null;
+        }
+        public string Recorrer()
+        {
+            string datos = string.Empty;
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                datos += nodoActual.Valor + "\n";
+            }
+            return datos;
+        }
+        public void AgregarFinal(string valor)
+        {
+            nodoActual = nodoInicial;
+           
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                
+            }
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Siguiente = nodoNuevo;
+        }
+
+        public void AgregarInicio(string valor)
+        {
+            if (ValidaVacio())
+            {
+                AgregarFinal(valor);
+                return;
+            }
+            Nodo nodoNuevo = new Nodo(valor, nodoInicial.Siguiente);
+
+            nodoInicial.Siguiente = nodoNuevo;
+        }
+        public Nodo Buscar(string valor)
+        {
+            if (ValidaVacio())
+            {
+                return null;
+            }
+            nodoActual = nodoInicial;
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+            }
+            return null;
+        }
+
+        public Nodo BuscarAnterior(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                Nodo nodoBusqueda = nodoInicial;
+
+                while (nodoBusqueda.Siguiente != null
+                            && nodoBusqueda.Siguiente.Valor != valor)
+                {
+                    nodoBusqueda = nodoBusqueda.Siguiente;
+                    if (nodoBusqueda.Siguiente.Valor == valor)
+                    {
+                        return nodoBusqueda;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void Borrar(string valor)
+        {
+            if (ValidaVacio() == false)
+            {
+                nodoActual = Buscar(valor);
+
+                if (nodoActual != null)
+                {
+                    Nodo nodoAnterior = BuscarAnterior(valor);
+                    nodoAnterior.Siguiente = nodoActual.Siguiente;
+                    nodoActual.Siguiente = null;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*se hace la colección
+            */
+            
+            
+            Lista nodoA = new Lista();            
+            Lista nodoB = new Lista();            
+            Lista nodoC = new Lista(); 
+
+            nodoA.AgregarFinal("X");
+            nodoA.AgregarFinal("Y");
+            nodoB.AgregarFinal("W");
+            nodoC.AgregarFinal("M");
+
+            List<String> coleccion = new List<String>
+            {
+                nodoA.Llave(),
+                nodoB.Llave(),
+                nodoC.Llave()
+            };
+
+
+            Console.WriteLine("Valores en la colección");
+            
+            foreach (String s in coleccion) {  Console.WriteLine(s); }
+            
+            Console.WriteLine("Valores en nodoA");
+            Console.WriteLine(nodoA.Recorrer());
+            Console.WriteLine("Valores en nodoB");
+            Console.WriteLine(nodoB.Recorrer());
+            Console.WriteLine("Valores en nodoC");
+            Console.WriteLine(nodoC.Recorrer());
+
+            /*
+            Lista lista = new Lista();
+
+            Console.WriteLine("Se agregan valores");
+            lista.AgregarFinal("Uno");
+            lista.AgregarFinal("Dos");
+            lista.AgregarFinal("Tres");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Se borra Dos");
+            lista.Borrar("Dos");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Se Agrega Cero al inicio");
+            lista.AgregarInicio("Cero");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Vaciar lista");
+            lista.VaciarLista();
+
+            Console.WriteLine("Se agregan nuevos valores");
+            lista.AgregarFinal("Cuatro");
+            lista.AgregarFinal("Cinco");
+            lista.AgregarFinal("Seis");
+            Console.WriteLine(lista.Recorrer());
+
+
+            Console.WriteLine("Valor anterior de Cinco");
+            Console.WriteLine(lista.BuscarAnterior("Cinco").Valor);
+            */
+            Console.ReadKey();
+        }
+}
+}
